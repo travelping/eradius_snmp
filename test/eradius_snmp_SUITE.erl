cb599901733acd0181648463ac60443025643ccb
Yury Gargay <yury.gargay@gmail.com>
2015-10-19 16:17:52 +0600

    Add rebar and simple test

---
+++ test/eradius_snmp_SUITE.erl
@@ -0,0 +1,37 @@
+-module(eradius_snmp_SUITE).
+-compile(export_all).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+%%%===================================================================
+%%% Common Test callbacks
+%%%===================================================================
+
+all() ->
+    [snmpa_info].
+
+init_per_suite(Config) ->
+    DbDir = string:strip(os:cmd("mktemp -d -t eradius_dnmp_tests.XXX"), right, $\n),
+    application:set_env(snmp, agent, [{config, [{dir, "../../sample/snmp/agent/conf/"}]},
+                                      {db_dir, DbDir},
+                                      {mibs, ["../../priv/mibs/ERADIUS-MIB"]}]),
+    application:ensure_all_started(eradius_snmp),
+    Config.
+
+end_per_suite(_Config) ->
+    application:stop(eradius_snmp),
+    ok.
+
+%%%===================================================================
+%%% Test cases
+%%%===================================================================
+
+snmpa_info(_Config) ->
+    Info = snmpa:info(),
+    ?assertEqual([v1, v2, v3], proplists:get_value(vsns, Info)),
+    MIBServer = proplists:get_value(mib_server, Info, []),
+    ct:pal("~p~n", [proplists:get_value(loaded_mibs, MIBServer)]),
+    ?assertEqual({'ERADIUS-MIB', true, "../../priv/mibs/ERADIUS-MIB.bin"}, 
+                 lists:keyfind('ERADIUS-MIB', 1, proplists:get_value(loaded_mibs, MIBServer))),
+    ok.
